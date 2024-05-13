858edb65165defec2b0ad9b9753eee05757c6da7
NaimoonJannat <naimoonjannat@gmail.com>
2024-05-13 12:29:58 +0600

    Firebase authentication added

---
+++ src/components/providers/AuthProvider.jsx
@@ -0,0 +1,86 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { GoogleAuthProvider } from "firebase/auth";
+import { GithubAuthProvider } from "firebase/auth";
+
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const provider = new GoogleAuthProvider();
+    const providerGit = new GithubAuthProvider();
+
+    const createUser = (name, email, photo, password) => {
+        setLoading(true);
+        // Create user
+        return createUserWithEmailAndPassword(auth, email, password)
+            .then(userCredential => {
+                const user = userCredential.user;
+    
+                // Update the profile with the provided name and photoURL
+                return updateProfile(user, {
+                    displayName: name,
+                    photoURL: photo
+                }).then(() => userCredential); // Return userCredential after profile update
+            })
+            .catch(error => {
+                throw error;
+            });
+    }
+    
+    
+    const signInGoogle = () =>{
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+
+    const signInGithub = () =>{
+        setLoading(true);
+        return signInWithPopup(auth, providerGit);
+    }
+
+    const signIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unSubscribe =
+        onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    },[])
+    const authInfo ={
+        user, 
+        loading,
+        createUser,
+        logOut,
+        signIn,
+        signInGoogle,
+        signInGithub
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import {
   RouterProvider, 
  } from "react-router-dom"; 
 import router from './Routes/Route.jsx';
+import AuthProvider from './components/providers/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} /> 
+    <AuthProvider><RouterProvider router={router} /> </AuthProvider>
   </React.StrictMode>,
 )
