797dde9b107f1ea35b45b43da01b074cd3f99778
NaimoonJannat <naimoonjannat@gmail.com>
2024-05-15 15:44:53 +0600

    Banner done fixing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Banner = () => {
@@ -11,7 +12,7 @@ const Banner = () => {
                 <p className="mt-6 mb-8 text-lg sm:mb-12"> It encapsulates the essence of your website, emphasizing collaboration, empowerment, and the convenience of group studying.
                 </p>
                 <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                    <button className="px-8 py-3 text-lg font-semibold rounded bg-[#F50000] ">Explore Now</button>
+                    <Link to="/assignments" className="px-8 btn py-3 text-lg font-semibold rounded bg-[#F50000] ">Explore Now</Link>
                 </div>
             </div>
             <div className="flex shrink-0 items-center shadow-[#F50000] shadow-2xl justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
