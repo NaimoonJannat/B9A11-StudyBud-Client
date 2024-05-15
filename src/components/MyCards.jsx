70758a7f2aa477882fe3c80be84788dc136dac29
NaimoonJannat <naimoonjannat@gmail.com>
2024-05-15 12:31:45 +0600

    Iframe fixed

--- src/components/MyCards.jsx
+++ src/components/MyCards.jsx
@@ -23,7 +23,7 @@ const MyCards = ({ filteredTask }) => {
                     <p>Feedback: {feedback}</p>
                 </div>
                 <div className="w-full md:w-1/3">
-                <button className="bg-[#F50000] mx-auto md:my-10 btn font-bold py-2 px-4 rounded" onClick={openModal}>Preview</button>
+                <button className="bg-[#F50000] md:my-14 btn font-bold py-2 px-4 rounded" onClick={openModal}>Preview</button>
                 </div>
             </div>
 
@@ -31,7 +31,7 @@ const MyCards = ({ filteredTask }) => {
                 <div className="fixed z-10 inset-0 overflow-y-auto">
                     <div className="flex items-center justify-center min-h-screen">
                         <div className="fixed inset-0 bg-black opacity-50" onClick={closeModal}></div>
-                        <div className="bg-white p-8 rounded-lg max-w-lg relative">
+                        <div className="bg-[#f500008d] border-2 border-[#F50000] p-8 rounded-lg w-11/12 md:w-1/2 relative">
                             <button className="absolute top-2 right-2 btn btn-circle" onClick={closeModal}>X</button>
                             <h2 className="text-lg font-semibold">Document Preview</h2>
                             <div className="mt-4">
